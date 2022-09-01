5325451249ab73062abf9da0923179cb089af2a9
bbayong28 <ghkdud5795@hanmail.net>
2022-09-01 10:02:39 +0900

    20220901

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//window.addEventListener('DOMContentLoaded', () => {});
+//$(function () { });
+
+//js -> document.querySelector('.Wrap').style.color = 'red'
+//jquery -> $('.Wrap').css("color","red")  /  $('.Wrap').css({color:'red'})
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    ///////////////////////////////////////////////////
+
+
+    //document.querySelectorAll('.Wrap').style.color = 'red'
+    console.log(document.querySelectorAll('.Wrap'))
+
+
+
+
+
+
+    ///////////////////////////////////////////////////
+
+});
